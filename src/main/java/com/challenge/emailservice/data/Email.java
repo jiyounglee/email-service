74539f44fe81d4998a9c1ab8294c8708a3f0916f
Ji Young Lee <jylee1103@gmail.com>
2018-06-24 19:58:03 +1000

    Added more validation for email payload

--- src/main/java/com/challenge/emailservice/data/Email.java
+++ src/main/java/com/challenge/emailservice/data/Email.java
@@ -1,15 +1,23 @@
 package com.challenge.emailservice.data;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.List;
 
 public class Email {
-    
-    private List<EmailAddress> tos;
-    private List<EmailAddress> froms;
-    private List<EmailAddress> ccs;
-    private List<EmailAddress> bccs;
+
+    @Valid
+    private List<@Valid EmailAddress> from;
+
+    @Valid
+    private List<@Valid EmailAddress> to;
+
+    @Valid
+    private List<@Valid EmailAddress> cc;
+
+    @Valid
+    private List<@Valid EmailAddress> bcc;
 
     @NotNull(message = "Subject is required")
     @Size(min = 1)
@@ -19,36 +27,36 @@ public class Email {
     @Size(min = 1)
     private String content;
 
-    public List<EmailAddress> getTos() {
-        return tos;
+    public List<EmailAddress> getFrom() {
+        return from;
     }
 
-    public void setTos(List<EmailAddress> tos) {
-        this.tos = tos;
+    public void setFrom(List<EmailAddress> from) {
+        this.from = from;
     }
 
-    public List<EmailAddress> getFroms() {
-        return froms;
+    public List<EmailAddress> getTo() {
+        return to;
     }
 
-    public void setFroms(List<EmailAddress> froms) {
-        this.froms = froms;
+    public void setTo(List<EmailAddress> to) {
+        this.to = to;
     }
 
-    public List<EmailAddress> getCcs() {
-        return ccs;
+    public List<EmailAddress> getCc() {
+        return cc;
     }
 
-    public void setCcs(List<EmailAddress> ccs) {
-        this.ccs = ccs;
+    public void setCc(List<EmailAddress> cc) {
+        this.cc = cc;
     }
 
-    public List<EmailAddress> getBccs() {
-        return bccs;
+    public List<EmailAddress> getBcc() {
+        return bcc;
     }
 
-    public void setBccs(List<EmailAddress> bccs) {
-        this.bccs = bccs;
+    public void setBcc(List<EmailAddress> bcc) {
+        this.bcc = bcc;
     }
 
     public String getSubject() {
--- src/main/java/com/challenge/emailservice/data/EmailAddress.java
+++ src/main/java/com/challenge/emailservice/data/EmailAddress.java
@@ -1,8 +1,14 @@
 package com.challenge.emailservice.data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+
 public class EmailAddress {
 
     private String name;
+
+    @Email
+    @Size(max = 320)
     private String address;
 
     public String getName() {
